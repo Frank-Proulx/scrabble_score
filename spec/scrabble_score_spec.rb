fde957b680199e2ff05589009f0198315c2bff8d
Frank-Proulx <frankproulx7@gmail.com>
2021-12-07 09:18:37 -0800

    add scrabbler class and initialize/scrabble method after test passing
    Co-authored-by: Marisa Edgar <marisaedgar1212@gmail.com>
    Co-authored-by: Matt Casperson <mc.casperson@gmail.com>

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -1,3 +1,9 @@
 require('rspec')
 require('scrabble_score')
 
+describe('Scrabbler#scrabble_score') do
+  it("returns a scrabble score for a letter") do
+    game = Scrabbler.new()
+    expect(game.scrabble('a')).to(eq(1))
+  end
+end
